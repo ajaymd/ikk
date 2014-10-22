8493b778b11ed29c4f7fbedcdc8d81d0d732d864
Yuval Scharf <yuval@quixey.com>
2014-10-21 21:06:14 -0700

    Permutations

---
+++ Recursion/permutations.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+ *
+ *  Print all permutations of length n
+ *
+ *****************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+void printPermutationsRecursion(int n, int *array, int i) {
+    if (i == n) {
+        for(int j = 0; j < n; ++j)
+            printf("%d ", array[j]);
+        printf("\n");
+        return;
+    }
+
+    for(int j = i; j < n; ++j) {
+        int tmp = array[j];
+        array[j] = array[i];
+        array[i] = tmp;
+        printPermutationsRecursion(n, array, i + 1);
+        array[i] = array[j];
+        array[j] = tmp;
+    } 
+}
+
+void printPermutations(int n) {
+    int *array = malloc(n * sizeof(int));
+    for(int i = 0; i < n; ++i)
+        array[i] = i + 1;
+    printPermutationsRecursion(n, array, 0);
+    free(array);
+}
+
+int main(int arcg, char **argv) {
+    printPermutations(atoi(argv[1]));
+    return 0;
+} 
+
+  
