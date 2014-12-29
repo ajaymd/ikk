a7e5dae4767c3aae015b42b480105adc7d18fffb
Yuval Scharf <yuval@quixey.com>
2014-12-29 15:13:35 -0800

    A small improvement to dp/rope

--- DynamicProgramming/rope.c
+++ DynamicProgramming/rope.c
@@ -14,19 +14,19 @@ int recursion(int ropeLength, int mustCut) {
   return max;
 }
 
-int dynamicProgramming(ropeLength) {
-  int *max = malloc(sizeof(int) * (ropeLength + 1));
+long dynamicProgramming(ropeLength) {
+  long *max = malloc(sizeof(long) * (ropeLength + 1));
 
   max[1] = 1;
 
   for(int j = 2; j <= ropeLength; ++j) { // j is the ropeLength on the recursion
-    int m = (j == ropeLength ?  0 : j);
+    long m = (j == ropeLength ?  0 : j);
     for(int i = 1; i < j; ++i) // i has the same role as in the recursion 
       m = MAX(m, max[i] * max[j - i]);
     max[j] = m;
   }
 
-  int result = max[ropeLength];
+  long result = max[ropeLength];
 
   free(max);
 
@@ -35,8 +35,8 @@ int dynamicProgramming(ropeLength) {
 
 int main() {
   int ropeLength = 13;
-  printf("Recursion: %d\n", recursion(13, 1));
-  printf("Dynamic Programming: %d\n", dynamicProgramming(13));
+  printf("Recursion: %d\n", recursion(ropeLength, 1));
+  printf("Dynamic Programming: %ld\n", dynamicProgramming(ropeLength));
   return 0;
 }
     
