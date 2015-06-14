a1bd07bf881973b905cbbd6c25f9d3f49c336f3c
Yuval Scharf <yuval@quixey.com>
2015-06-13 23:40:07 -0700

    Popular Element

---
+++ Arrays/PopularElement.java
@@ -0,0 +1,35 @@
+public class PopularElement {
+    static int[] a;
+
+    public static void main(String[] args) {
+        if (args.length < 4)
+            return;
+
+        a = new int[args.length];
+        for (int i = 0; i < a.length; ++i)
+            a[i] = Integer.valueOf(args[i]);
+
+        int p = a.length / 4;
+        if (p * 4 < a.length)
+            p++;
+
+        for (int i = 1; i <= 4; ++i) {
+            int candidate = a[p * i  - 1];
+            int l = findLeftMostElement(candidate, p * (i - 1), p * i - 1);
+            if (l + p - 1 >= a.length)
+                return;
+            if (a[l + p - 1] == candidate) {
+                System.out.println(candidate);
+                return;
+            }
+        }
+    }
+
+    static int findLeftMostElement(int element, int left, int right) {
+        if (left == right)
+            return left;
+        int mid = (left + right) / 2;
+        return a[mid] >= element ? findLeftMostElement(element, left, mid)
+                                 : findLeftMostElement(element, mid + 1, right);
+    }
+}
