fa7f0ef226148723d5c1e74d2a93e9b5f034e5cf
gayatri2307 <gayatri2307@gmail.com>
2016-09-19 16:18:06 -0700

    Trees function definitions
    Common utility functions needed

---
+++ Trees/trees.c
@@ -0,0 +1,16 @@
+/*
+ * trees.c
+ *
+ *      Author: Gayatri
+ */
+
+
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "trees.h"
+
+int min(int a, int b) {
+	return ((a < b) ? a : b);
+}
