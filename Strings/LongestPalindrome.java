f8b39c08d051d3e97a15ecae19e3fd6212c55b71
Yuval Scharf <yuval@quixey.com>
2016-10-30 02:33:43 -0700

    Improve "LongestPalindrome"

--- Strings/LongestPalindrome.java
+++ Strings/LongestPalindrome.java
@@ -40,16 +40,25 @@ public class LongestPalindrome {
         Pair max = new Pair(0, 0);
 
         for (int c = 0; c < s.length() - 1; ++c) {
-            int b, e;
-            for (b = c - 1, e = c + 1; b >= 0 && e < s.length() && s.charAt(b) == s.charAt(e); --b, ++e)
-                ;
-            if ((e - 1) - (b + 1) + 1 > max.e - max.b + 1)
-                max = new Pair(b + 1, e - 1);
+            int b = c - 1;
+            int e = c + 1;
+            while (b >= 0 && e < s.length() && s.charAt(b) == s.charAt(e)) {
+                --b;
+                ++e;
+            }
+            ++b; --e;
+            if (e - b + 1 > max.e - max.b + 1)
+                max = new Pair(b, e);
 
-            for (b = c, e = c + 1; b >= 0 && e < s.length() && s.charAt(b) == s.charAt(e); --b, ++e)
-                ;
-            if ((e - 1) - (b + 1) + 1 > max.e - max.b + 1)
-                max = new Pair(b + 1, e - 1);
+            b = c;
+            e = c + 1;
+            while (b >= 0 && e < s.length() && s.charAt(b) == s.charAt(e)) {
+                --b;
+                ++e;
+            }
+            ++b; --e;
+            if (e - b + 1 > max.e - max.b + 1)
+                max = new Pair(b, e);
         }
 
         return max;
@@ -60,45 +69,49 @@ public class LongestPalindrome {
         if (s.length() == 0)
             return null;
 
-        int[] p = new int[2 * s.length() - 1]; // Maintain the wing length
-        p[0] = 0;
-        int leader = 0;
-        int max = 0;                           // Center of maximum palindrome
+        if (s.length() == 1)
+            return new Pair(0, 0);
 
-        for (int c = 1; c < 2 * s.length() - 1; ++c) {
-            int b, e;
-            int w = 0;
+        int[] w = new int[2 * s.length() - 1]; // Maintain the wing lengths
+        if (s.charAt(0) == s.charAt(1))
+            w[1] = 1;
+        int max = w[1] == 1 ? 1 : 0;           // Index of the center of maximum palindrome
+        int leader = 1;                        // Index of the center of the leader
 
+        int e = 2;
+        for (int c = 2; c < w.length; ++c) {
             int buddy = leader - (c - leader);
-            if (buddy >= 0) {
-                int buddyLeftTip = buddy / 2 - p[buddy] + (buddy % 2 == 0 ? 0 : 1);
-                int leaderLeftTip = leader / 2 - p[leader] + (leader % 2 == 0 ? 0 : 1);
-                if (buddyLeftTip > leaderLeftTip) { // We spend only O(1) on this center. Thank you Mancher.
-                    if (c / 2 + p[buddy] > s.length())
-                        p[c] = s.length() - c / 2;      // The palindrome is chopped by the end of the string.
-                    else                                // We continue the algorithm because sometime we want the lengths
-                        p[c] = p[buddy];                // of the palindromes arounf all centers.
-                    continue;
-                } else {
-                    w = buddy / 2 - leaderLeftTip + (buddy % 2 == 0 ? 0 : 1);
-                }
+            if (getLeftTip(buddy, w[buddy]) > getLeftTip(leader, w[leader])) {
+                w[c] = Math.min(w[buddy], (s.length() - 1) - c / 2);
+                continue;   // We spend only O(1) on this center. Thank you Mancher.
             }
 
-            b = c / 2 - w - (c % 2 == 0 ? 1 : 0);   // If w > 0 we still gain from Mancher.
-            e = c / 2 + w + 1;
+            int b = getLeftTip(c, e - c / 2);
             while (b >= 0 && e < s.length() && s.charAt(b) == s.charAt(e)) {
                 --b;
                 ++e;
             }
 
             int l = (e - 1) - (b + 1) + 1;
-            if (l > 2 * p[max] + (max % 2 == 0 ? 1: 0))
+            if (l > getRightTip(max, w[max]) - getLeftTip(max, w[max]) + 1)
                 max = c;
-            p[c] = l / 2;
+            w[c] = l / 2;
             leader = c;
         }
 
-        return new Pair(max / 2 - p[max] + (max % 2 == 0 ? 0 : 1), max / 2 + p[max]);
+        return new Pair(getLeftTip(max, w[max]), getRightTip(max, w[max]));
+    }
+
+    // The two functions below return incorrect answers for (indexOfCenter % 2 == 1 && wing == 0)
+    // but the function above still works correctly with those answers.
+
+    private static int getLeftTip(int indexOfCenter, int wing) {
+        int t = indexOfCenter / 2 - wing;
+        return  indexOfCenter % 2 == 1 ? t + 1 : t;
+    }
+
+    private static int getRightTip(int indexOfCenter, int wing) {
+        return indexOfCenter / 2 + wing;
     }
 
     public static void main(String[] args) {
