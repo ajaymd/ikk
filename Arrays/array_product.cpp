e80fc80989d18e381e5aba178e9b5518f71aaf0e
Niloy Datta <juniloydatta@gmail.com>
2017-03-30 21:19:37 +0600

    Added problem description and complexity

--- Arrays/array_product.cpp
+++ Arrays/array_product.cpp
@@ -1,3 +1,15 @@
+/**
+ * ***************** PROBLEM DESCRIPTION ************************
+ * Given an array of numbers, nums, return an array of numbers products, where products[i]is the product of all nums[j], j != i.
+
+Input : [1, 2, 3, 4, 5]
+Output: [(2*3*4*5), (1*3*4*5), (1*2*4*5), (1*2*3*5), (1*2*3*4)]
+      = [120, 60, 40, 30, 24]
+You must do this in O(N) time, and constant space, without using division. Usage of products array is not considered extra space.
+
+Without using division is the key constraint to remember.
+*/
+
 #include<iostream>
 #include<vector>
 
@@ -33,4 +45,7 @@ int main(){
 	cout<<endl;
 	return 0;
 }
-
+/**
+ * Time complexity: O(2*N)
+ * Space complexity: O(N)
+*/
