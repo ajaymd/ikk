ea276d2050883c9b0f5079555d99a3ac2e72565e
Niloy Datta <juniloydatta@gmail.com>
2017-04-16 23:23:37 +0600

    Added problem description and complexity inside java and C++ solution for problem 'Double Power'

--- Recursion/DoublePower.cpp
+++ Recursion/DoublePower.cpp
@@ -1,3 +1,23 @@
+/**
+	Problem descrption:
+
+
+Implement a power function to raise a double to an int power, including negative powers.
+ 
+e.g. pow(double d, int p) should give 'd' raised to power 'p'.
+ 
+Of course, please don't use in-built methods like pow(). Idea is to implement that using recursion.
+ 
+ 
+Solution: http://stackoverflow.com/questions/101439/the-most-efficient-way-to-implement-an-integer-based-power-function-powint-int
+ 
+Suggested time: 10 minutes to do a brute-force and 15 with a trick that optimizes it.
+ 
+Note: In HackerRank, Double is simulated with "float".
+
+
+*/
+
 #include<iostream>
 using namespace std;
 
@@ -23,4 +43,7 @@ int main(){
 	cout<<resultValue<<endl;
 	return 0;
 }
-
+/*
+	Time compelxity: log(N) where N is the exponent value
+	Space compelxity: O(1)
+*/
--- Recursion/DoublePower.java
+++ Recursion/DoublePower.java
@@ -1,3 +1,22 @@
+/**
+	Problem descrption:
+
+
+Implement a power function to raise a double to an int power, including negative powers.
+ 
+e.g. pow(double d, int p) should give 'd' raised to power 'p'.
+ 
+Of course, please don't use in-built methods like pow(). Idea is to implement that using recursion.
+ 
+ 
+Solution: http://stackoverflow.com/questions/101439/the-most-efficient-way-to-implement-an-integer-based-power-function-powint-int
+ 
+Suggested time: 10 minutes to do a brute-force and 15 with a trick that optimizes it.
+ 
+Note: In HackerRank, Double is simulated with "float".
+
+
+*/
 import java.io.*;
 import java.util.*;
 
@@ -25,3 +44,7 @@ public class DoublePower{
 		System.out.println(resultValue);
 	}
 }
+/*
+	Time compelxity: log(N) where N is the exponent value
+	Space compelxity: O(1)
+*/
