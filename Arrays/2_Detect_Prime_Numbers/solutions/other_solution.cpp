62ffabfb311236ac5c73063ae95c1daf079dd9bd
Dipen Dadhaniya <dipendadhaniya@gmail.com>
2017-09-15 16:17:18 +0530

    Adding problem "Detect Prime Numbers"
    On hackerrank All Tests > Ad-hoc (Arrays) - Timed Test > Questions > Detect Prime Numbers
    
    Includes solution, problem statement, test cases, test cases generatos.

---
+++ Arrays/2_Detect_Prime_Numbers/solutions/other_solution.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+	//freopen("..//test_cases//sample_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//sample_test_cases_output.txt", "w", stdout);
+	//freopen("..//test_cases//handmade_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//handmade_test_cases_output.txt", "w", stdout);
+	//freopen("..//test_cases//small_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//small_test_cases_output.txt", "w", stdout);
+	//freopen("..//test_cases//big_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//big_test_cases_output.txt", "w", stdout);
+	//freopen("..//test_cases//ignore.txt", "w", stdout);
+
+
+
+	return 0;
+}
