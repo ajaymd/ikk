6dd644515a142fb726deb8a5d5402ae64085cede
Dipen Dadhaniya <dipendadhaniya@gmail.com>
2017-09-21 13:13:10 +0530

    Adding problem "Divide An Integer By Another Integer"
    Also added time complexity and space complexity in editorial of problem number 3 "Complete Read(N) Using Already Given Read_K()".
    
    On hackerrank All Tests > Ad-hoc (Arrays) - Timed Test > Questions > Divide An Integer By Another Integer
    
    Includes solutions, problem statement, test cases, editorial, etc.

---
+++ Arrays/6_Divide_An_Integer_By_Another_Integer/solutions/optimal_solution.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long long int divide(long long int a, long long int b)
+{
+	if (a == 0)											// 0 / non_zero = 0
+	{
+		return 0;
+	}
+	if ((a < 0 && b > 0) || (a > 0 && b < 0))			// neg / pos or pos / neg
+	{
+		return -divide(abs(a), abs(b));
+	}
+	if (a < 0 && b < 0)									// neg / neg
+	{
+		return divide(-a, -b);
+	}
+	if (a < b)											// like 2 / 5
+	{
+		return 0;
+	}
+	long long int q = 1;								// long long int is imp
+	long long int b_copy = b;							// long long int is imp
+	while (b_copy << 1 <= a)							// 37 / 3 can be written as 8 * (37 / (3 * 8)) + (37 - (3 * 8)) / 3
+	{
+		b_copy <<= 1;
+		q <<= 1;
+	}
+	return q + divide(a - b_copy, b);
+}
+
+int main()
+{
+	//freopen("..//test_cases//sample_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//sample_test_cases_output.txt", "w", stdout);
+	freopen("..//test_cases//handmade_test_cases_input.txt", "r", stdin);
+	freopen("..//test_cases//handmade_test_cases_output.txt", "w", stdout);
+	//freopen("..//test_cases//small_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//small_test_cases_output.txt", "w", stdout);
+	//freopen("..//test_cases//big_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//big_test_cases_output.txt", "w", stdout);
+	//freopen("..//test_cases//ignore.txt", "w", stdout);
+
+	int test_cases;
+	cin >> test_cases;
+	while (test_cases--)
+	{
+		long long int a, b;
+		cin >> a >> b;
+		assert(INT_MIN <= a);
+		assert(a <= INT_MAX);
+		assert(INT_MIN <= b);
+		assert(b <= INT_MAX);
+		assert(b != 0);
+		long long int ans = divide(a, b);
+		assert(ans == a / b);
+		cout << ans << endl;
+	}
+
+	return 0;
+}
