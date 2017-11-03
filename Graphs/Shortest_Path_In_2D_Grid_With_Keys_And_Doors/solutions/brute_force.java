3f23b55de2cf8ebaafa789f444dfd9a084354b0d
Dipen Dadhaniya <dipendadhaniya@gmail.com>
2017-11-03 15:50:23 +0530

    Adding problem "Shortest Path In 2D Grid With Keys And Doors"
    From graphs test.
    
    Contains problem statement, test cases, solutions, editorial, test case generators, test case parser etc.

---
+++ Graphs/Shortest_Path_In_2D_Grid_With_Keys_And_Doors/solutions/brute_force.java
@@ -0,0 +1,141 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the function below.
+     */
+    static int[][] find_shortest_path(String[] grid) {
+    	char [][] board = new char[grid.length][grid[0].length()];
+    	for(int i = 0; i < grid.length; i++)
+    	{
+    		for (int j = 0; j < grid[0].length(); j++)
+    		{
+    			board[i][j] = grid[i].charAt(j);
+                //System.out.println(board[i][j]);
+    		}
+    	}
+        return (pathToString(solve(board)));
+    }
+
+    private static final class Coord {
+		final int y, x;
+		Coord(int y, int x) {
+			this.y = y;
+			this.x = x;
+		}
+		@Override public String toString() {
+			return String.valueOf(this.y).concat(String.valueOf(this.x));
+		}
+	}
+	private static int[][] pathToString(Iterable<Coord> path) {
+        int sz = 0;
+		for (Coord coord : path)
+            sz++;
+        //System.out.println(sz);
+        int[][] ans = new int[sz][2];
+        int i = 0;
+        for (Coord coord : path)
+        {
+            ans[i][0] = coord.y;
+            ans[i][1] = coord.x;
+            i++;
+        }
+		return ans;
+	}
+	private static List<Coord> solve(char[][] board) {
+		List<Coord> shortestPathToGoal = new ArrayList<>();
+		for (int y = 0; y < board.length; y++)
+			for (int x = 0; x < board[y].length; x++)
+            {
+                // System.out.println(board[y][x]);
+				if (board[y][x] == '@'/*Start*/)
+					walk(board, y, x, 0, initVisited(board), new ArrayDeque<>(), shortestPathToGoal);
+            }
+		return shortestPathToGoal;
+	}
+	private static void walk(char[][] board, int y, int x, int keyRing, boolean[][] visited,
+	                         Deque<Coord> path, List<Coord> shortestPathToGoal) {
+        //System.out.println(y);
+		if (y < 0 || y >= board.length || x < 0 || x >= board[y].length || visited[y][x])
+			return; // outside board or already visited
+		char point = board[y][x];
+		if (point == '#'/*Water*/)
+			return; // cannot walk on water
+		if (point == '+'/*Goal*/) {
+			shortestPathToGoal.clear();
+			shortestPathToGoal.addAll(path);
+			shortestPathToGoal.add(new Coord(y, x));
+            //System.out.println("---- ----");
+			return; // return to look for alternate shorter path
+		}
+		if (! shortestPathToGoal.isEmpty() && path.size() + 2 >= shortestPathToGoal.size())
+			return; // shorter (or equal) path already found
+		if (point >= 'A' && point <= 'Z') { // Door
+			if ((keyRing & (1 << (point - 'A'))) == 0)
+				return; // missing key
+		} else if (point >= 'a' && point <= 'z') { // Key
+			if ((keyRing & (1 << (point - 'a'))) == 0) {
+				keyRing |= (1 << (point - 'a')); // add key to keyring
+				visited = initVisited(board); // may now revisit previous steps
+			}
+		}
+		visited[y][x] = true;
+		path.addLast(new Coord(y, x));
+		walk(board, y    , x + 1, keyRing, visited, path, shortestPathToGoal); // right
+		walk(board, y + 1, x    , keyRing, visited, path, shortestPathToGoal); // down
+		walk(board, y    , x - 1, keyRing, visited, path, shortestPathToGoal); // left
+		walk(board, y - 1, x    , keyRing, visited, path, shortestPathToGoal); // up
+		path.removeLast();
+		visited[y][x] = false;
+	}
+	private static boolean[][] initVisited(char[][] board) {
+		boolean[][] visited = new boolean[board.length][];
+		for (int y = 0; y < board.length; y++)
+			visited[y] = new boolean[board[y].length];
+		return visited;
+	}
+
+	public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        final String fileName = System.getenv("OUTPUT_PATH");
+        BufferedWriter bw = null;
+        if (fileName != null) {
+            bw = new BufferedWriter(new FileWriter(fileName));
+        }
+        else {
+            bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        int[][] res;
+        int grid_size = 0;
+        grid_size = Integer.parseInt(in.nextLine().trim());
+
+        String[] grid = new String[grid_size];
+        for(int i = 0; i < grid_size; i++) {
+            String grid_item;
+            try {
+                grid_item = in.nextLine();
+            } catch (Exception e) {
+                grid_item = null;
+            }
+            grid[i] = grid_item;
+        }
+
+        res = find_shortest_path(grid);
+        int res_rowLength = res.length;
+        int res_colLength = res[0].length;
+        for(int res_i = 0; res_i < res_rowLength; res_i++) {
+            for(int res_j = 0; res_j < res_colLength; res_j++) {
+                bw.write(String.valueOf(res[res_i][res_j]) + " ");
+            }
+            bw.newLine();
+        }
+
+        bw.close();
+    }
+}
---
+++ Graphs/Shortest_Path_In_2D_Grid_With_Keys_And_Doors/solutions/optimal_solution.cpp
@@ -0,0 +1,264 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// ---- START ----
+
+const int MAX_ROWS = 100, MAX_COLS = 100, MAX_KEYS = 10, MAX_MASK = (1 << MAX_KEYS), INF = MAX_ROWS * MAX_COLS * (MAX_KEYS + 1);
+const int add_r[4] = {-1, 0, 1, 0};
+const int add_c[4] = {0, -1, 0, 1};
+
+vector<vector<int>> build_path(vector<vector<vector<int>>> &dp, vector<vector<vector<pair<pair<int, int>, int>>>> &parent, int ring_of_keys, pair<int, int> &start, pair<int, int> &stop)
+{
+	vector<vector<int>> ans;
+	ans.push_back(vector<int>(0));
+	ans[0].push_back(stop.first);
+	ans[0].push_back(stop.second);
+	// cout << " - dp = " << dp[stop.first][stop.second][ring_of_keys] << " (" << stop.first << ", " << stop.second << ") ring = " << ring_of_keys << endl;
+	// cout << " - " << ans.size() << endl;
+
+	// stop only when ring_of_keys is 0. Both conditions are necessary. Consider input 1 5 "#a@A+#", here @ -> a -> @ -> A -> + , so when reconstructing + -> A -> @ and still we need to continue! 
+	while (stop != start || ring_of_keys != 0)
+	{
+		pair<pair<int, int>, int> par = parent[stop.first][stop.second][ring_of_keys];
+		stop = par.first;
+		ring_of_keys = par.second;
+		ans.push_back(vector<int>(0));
+		ans[ans.size() - 1].push_back(stop.first);
+		ans[ans.size() - 1].push_back(stop.second);
+		// cout << " - dp = " << dp[stop.first][stop.second][ring_of_keys] << " (" << stop.first << ", " << stop.second << ") ring = " << ring_of_keys << endl;
+		// cout << " - " << ans.size() << endl;
+	}
+	reverse(ans.begin(), ans.end());
+	return ans;
+}
+
+bool is_start(char ch)
+{
+	return ch == '@';
+}
+
+bool is_stop(char ch)
+{
+	return ch == '+';
+}
+
+bool is_water(char ch)
+{
+	return ch == '#';
+}
+
+bool is_land(char ch)
+{
+	return ch == '.';
+}
+
+bool is_key(char ch)
+{
+	return ('a' <= ch && ch < ('a' + MAX_KEYS));
+}
+
+bool is_door(char ch)
+{
+	return ('A' <= ch && ch < ('A' + MAX_KEYS));
+}
+
+bool can_open_door(char door, int ring_of_keys)
+{
+	return (ring_of_keys >> (door - 'A')) & 1;
+}
+
+void add_neighbour_to_queue(int to_r, int to_c, int to_ring_of_keys, pair<pair<int, int>, int> from, vector<vector<vector<pair<pair<int, int>, int>>>> &parent, vector<vector<vector<int>>> &dp, vector<vector<vector<bool>>> &visited, queue<pair<pair<int, int>, int>> &q)
+{
+	parent[to_r][to_c][to_ring_of_keys] = from;
+	dp[to_r][to_c][to_ring_of_keys] = dp[from.first.first][from.first.second][from.second] + 1;
+	visited[to_r][to_c][to_ring_of_keys] = true;
+	q.push({{to_r, to_c}, to_ring_of_keys});
+}
+
+void bfs(vector<string> &grid, pair<int, int> &start, vector<vector<vector<int>>> &dp, vector<vector<vector<pair<pair<int, int>, int>>>> &parent, vector<vector<vector<bool>>> &visited)		// better to pass vectors by reference
+{
+	int rows = grid.size();
+	int cols = grid[0].length();
+
+	queue<pair<pair<int, int>, int>> q;
+	// Starting point with no keys has 0 distance (itself). 
+	q.push({start, 0});
+	dp[start.first][start.second][0] = 0;
+	visited[start.first][start.second][0] = true;
+
+	while(q.empty() == false)
+	{
+		pair<pair<int, int>, int> from = q.front();
+		q.pop();
+
+		// cout << "popped = (" << from.first.first << ", " << from.first.second << ") - ring = " << from.second << " length = " << dp[from.first.first][from.first.second][from.second] << endl;
+
+		if (is_stop(grid[from.first.first][from.first.second]))
+		{
+			continue;
+		}
+		// Visit all four neighbours and update.
+		for (int i = 0; i < 4; i++)
+		{
+			int to_r = from.first.first + add_r[i];
+			int to_c = from.first.second + add_c[i];
+			if (to_r < 0 || to_r >= rows || to_c < 0 || to_c >= cols)
+			{
+				continue;
+			}
+			if (is_water(grid[to_r][to_c]))
+			{
+				continue;
+			}
+			else if (is_land(grid[to_r][to_c]) || is_start(grid[to_r][to_c]) || is_stop(grid[to_r][to_c]))
+			{
+				if (visited[to_r][to_c][from.second] == false)
+				{
+					add_neighbour_to_queue(to_r, to_c, from.second, from, parent, dp, visited, q);
+				}
+			}
+			else if (is_key(grid[to_r][to_c]))
+			{
+				int new_ring_of_keys = from.second  | (1 << (grid[to_r][to_c] - 'a'));
+				if (visited[to_r][to_c][new_ring_of_keys] == false)
+				{
+					add_neighbour_to_queue(to_r, to_c, new_ring_of_keys, from, parent, dp, visited, q);
+				}
+			}
+			else if (is_door(grid[to_r][to_c]))
+			{
+				// Can enter only if we have key for that door. 
+				if (can_open_door(grid[to_r][to_c], from.second))
+				{
+					if (visited[to_r][to_c][from.second] == false)
+					{
+						add_neighbour_to_queue(to_r, to_c, from.second, from, parent, dp, visited, q);
+					}
+				}
+			}
+		}
+	}
+}
+
+void get_start_and_stop_positions(vector<string> &grid, pair<int, int> &start, pair<int, int> &stop)	// node that start and stop are passed by reference hence change will be reflected. 
+{
+	int rows = grid.size();
+	int cols = grid[0].length();
+	for (int r = 0; r < rows; r++)
+	{
+		for (int c = 0; c < cols; c++)
+		{
+			if (is_start(grid[r][c]))
+			{
+				start = {r, c};
+			}
+			else if(is_stop(grid[r][c]))
+			{
+				stop = {r, c};
+			}
+		}
+	}
+}
+
+vector<vector<int>> find_shortest_path(vector<string> grid)
+{
+	int rows = grid.size();
+	int cols = grid[0].length();
+	pair<int, int> start, stop;
+	// Get the starting and ending point.
+	get_start_and_stop_positions(grid, start, stop);
+
+	// dp[r][c][ring] denotes length of shortest path from starting point to (r, c) and we have already got keys in the ring. 
+	vector<vector<vector<int>>> dp(rows, vector<vector<int>>(cols, vector<int>(MAX_MASK, INF)));
+	// parent[r][c][ring] denotes the last node through which dp[r][c][ring] is updated. This will help to reconstruct the path.
+	vector<vector<vector<pair<pair<int, int>, int>>>> parent(rows, vector<vector<pair<pair<int, int>, int>>>(cols, vector<pair<pair<int, int>, int>>(MAX_MASK, {{-1, -1}, -1})));
+	// visited[r][c][ring] keeps track of dp[r][c][ring] is visited or not. Though parent is enough to track this, but for readability purpose I have added this also. 
+	vector<vector<vector<bool>>> visited(rows, vector<vector<bool>>(cols, vector<bool>(MAX_MASK, false)));
+	// Do bfs.
+	bfs(grid, start, dp, parent, visited);
+
+	// As dp[r][c][ring] denotes shortest path from start point and we have already got keys in the ring, we just need to find for which key path is shorter. 
+	int length = INF;
+	int ring_of_keys;
+	for (int i = 0; i < MAX_MASK; i++)
+	{
+		if (length > dp[stop.first][stop.second][i])
+		{
+			length = dp[stop.first][stop.second][i];
+			ring_of_keys = i;
+		}
+	}
+	// cout << length << endl;
+
+	// Build the path from using parent. 
+	return build_path(dp, parent, ring_of_keys, start, stop);
+}
+
+// ---- STOP ----
+
+
+int main()
+{
+	freopen("..//test_cases//sample_test_cases_input.txt", "r", stdin);
+	freopen("..//test_cases//sample_test_cases_expected_output.txt", "w", stdout);
+	//freopen("..//test_cases//handmade_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//handmade_test_cases_expected_output.txt", "w", stdout);
+	//freopen("..//test_cases//generated_small_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//generated_small_test_cases_expected_output.txt", "w", stdout);
+	//freopen("..//test_cases//generated_big_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//generated_big_test_cases_expected_output.txt", "w", stdout);
+	//freopen("..//test_cases//ignore.txt", "w", stdout);
+
+	int test_cases;
+	cin >> test_cases;
+	while (test_cases--)
+	{
+		int rows, cols;
+		cin >> rows >> cols;
+		assert(1 <= rows);
+		assert(rows <= MAX_ROWS);
+		assert(1 <= cols);
+		assert(cols <= MAX_COLS);
+
+		vector<string> grid (rows);
+		int start = 0, stop = 0;
+		for (int r = 0; r < rows; r++)
+		{
+			cin >> grid[r];
+			for (int c = 0; c < cols; c++)
+			{
+				assert(
+					is_start(grid[r][c]) ||
+					is_stop(grid[r][c])  ||
+					is_water(grid[r][c]) ||
+					is_land(grid[r][c])  ||
+					is_key(grid[r][c])   ||
+					is_door(grid[r][c])
+				);
+				if (is_start(grid[r][c]))
+				{
+					start++;
+				}
+				else if (is_stop(grid[r][c]))
+				{
+					stop++;
+				}
+			}
+		}
+		assert(start == 1);
+		assert(stop == 1);
+
+		vector<vector<int>> ans = find_shortest_path(grid);
+
+		cout << ans.size() << endl;
+		assert(ans.size() != 0);
+		/*for (int i = 0; i < ans.size(); i++)
+		{
+			cout << ans[i][0] << " " << ans[i][1] << endl;
+		}
+		cout << endl;*/
+	}
+
+	return 0;
+}
