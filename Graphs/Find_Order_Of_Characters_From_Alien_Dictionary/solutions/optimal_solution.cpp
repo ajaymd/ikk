ae02fb66d1b36c216e9dacade666b1babe714b40
Dipen Dadhaniya <dipendadhaniya@gmail.com>
2017-11-04 17:50:54 +0530

    Added comments in the solution of problem "Find Order Of Characters From Alien Dictionary"

--- Graphs/Find_Order_Of_Characters_From_Alien_Dictionary/solutions/optimal_solution.cpp
+++ Graphs/Find_Order_Of_Characters_From_Alien_Dictionary/solutions/optimal_solution.cpp
@@ -19,12 +19,14 @@ void dfs(char from, unordered_map<char, vector<char>> &adj_list, string &topolog
 	topological_order = from + topological_order;
 }
 
+// Topological sort. 
 string topological_sort(unordered_map<char, vector<char>> &adj_list)
 {
 	string topological_order = "";
 	unordered_set<char> visited;
 	for (auto it = adj_list.begin(); it != adj_list.end(); it++)
 	{
+		// If current node is not visited then visit it and visit nodes reachable from that node. 
 		if (visited.find(it->first) == visited.end())
 		{
 			dfs(it->first, adj_list, topological_order, visited);
@@ -36,8 +38,10 @@ string topological_sort(unordered_map<char, vector<char>> &adj_list)
 string find_order(vector<string> words)
 {
 	int n = words.size();
+	// Contains adjacent nodes. 
 	unordered_map<char, vector<char>> adj_list;
 	
+	// Initialize nodes with no edges. 
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < words[i].length(); j++)
@@ -46,6 +50,7 @@ string find_order(vector<string> words)
 		}
 	}
 
+	// Find mismatch and add edges. 
 	for (int i = 0; i < n - 1; i++)
 	{
 		int min_len = min(words[i].length(), words[i + 1].length());
@@ -70,6 +75,7 @@ void make_sure_unique_ordering_possible_dfs(char from, unordered_map<char, vecto
 	active.insert(from);
 	for (auto it = adj_list[from].begin(); it != adj_list[from].end(); it++)
 	{
+		// If active then it contains a cycle. 
 		assert(active.find(*it) == active.end());
 		if (visited.find(*it) == visited.end())
 		{
@@ -92,6 +98,8 @@ void make_sure_unique_ordering_possible_topological_sort(unordered_map<char, vec
 			make_sure_unique_ordering_possible_dfs(it->first, adj_list, topological_order, visited, active);
 		}
 	}
+
+	// If we want to get order uniquely then from starting node, each node will be at unique distance (0, 1, ..., n - 1). It is not possible that two nodes are at same distance, because if it is the case then we can change their order in output (not unique)! 
 	unordered_map<char, int> dist;
 
 	dist[topological_order[0]] = 0;
@@ -141,7 +149,9 @@ void make_sure_unique_ordering_possible(vector <string> &words, unordered_set<ch
 		}
 	}
 
+	// There should be exactly one node with no input edges.
 	assert(has_out_edge.size() == diff_chars.size() - 1);
+	// There should be exactly one node with no output edges.
 	assert(has_in_edge.size() == diff_chars.size() - 1);
 
 	make_sure_unique_ordering_possible_topological_sort(adj_list, diff_chars);
