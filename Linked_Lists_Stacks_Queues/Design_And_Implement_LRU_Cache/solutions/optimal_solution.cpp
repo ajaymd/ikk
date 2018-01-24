8cf4a657118c6e906a3d73decfb9bea03a2484d6
dipen <dipendadhaniya@gmail.com>
2018-01-24 17:18:17 +0530

    Adding "Design_And_Implement_LRU_Cache" in "Linked_Lists_Stacks_Queues".

---
+++ Linked_Lists_Stacks_Queues/Design_And_Implement_LRU_Cache/solutions/optimal_solution.cpp
@@ -0,0 +1,179 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int MAX_CAPACITY = 100000, MAX_N = 100000, MAX_KEY = 10000, MAX_VAL = 10000;
+
+// -------------------------- START --------------------------
+
+class LRU_cache
+{
+public:
+	// Maximum size of the cache.
+	int capacity;
+	// Stores the {key, value} pair.
+	list<pair<int, int>> actual_storage;
+	// key -> pointer to where it is stored in actual storage.
+	unordered_map<int, list<pair<int, int>>::iterator> key_to_actual_storage_mapping;
+
+	// Constructor.
+	LRU_cache(int _capacity)
+	{
+		capacity = _capacity;
+	}
+
+	/*
+	Add {key, value} pair at the front of actual_storage. 
+	Also add the mapping (key -> pointer to where it is stored in actual storage).
+	That is (key -> beginning of the actual_storage).
+	*/
+	void add_to_front(int key, int value)
+	{
+		actual_storage.push_front({key, value});
+		key_to_actual_storage_mapping[key] = actual_storage.begin();
+	}
+
+	/*
+	Remove one {key, value} pair from the end of actual_storage.
+	Also remove the mapping (key -> pointer to where it is stored in actual storage).
+	*/
+	void remove_least_recently_used()
+	{
+		int key = actual_storage.back().first;
+		key_to_actual_storage_mapping.erase(key);
+		actual_storage.pop_back();
+	}
+
+	// Return value of the key, if key is present, else return -1.
+	int get(int key)
+	{
+		unordered_map<int, list<pair<int, int>>::iterator>::iterator it = key_to_actual_storage_mapping.find(key);
+		// If key is not present in mapping then return -1.
+		if (it == key_to_actual_storage_mapping.end())
+		{
+			return -1;
+		}
+		/*
+		it->second points to {key, value} in the actual_storage.
+		So it->second->second will give value.
+		*/
+		int value = it->second->second;
+		// Remove from the original position.
+		actual_storage.erase(it->second);
+		// Add to the front.
+		add_to_front(key, value);
+		return value;
+	}
+
+	void set(int key, int value)
+	{
+		unordered_map<int, list<pair<int, int>>::iterator>::iterator it = key_to_actual_storage_mapping.find(key);
+		// If key is not present in mapping then add {key, value} and setup the mapping.
+		if (it == key_to_actual_storage_mapping.end())
+		{
+			// If cache is full then remove the least recently used {key, value}.
+			if (actual_storage.size() == capacity)
+			{
+				remove_least_recently_used();
+			}
+			// Add to the front.
+			add_to_front(key, value);
+			return;
+		}
+		// Remove from the original position.
+		actual_storage.erase(it->second);
+		// Add to the front.
+		add_to_front(key, value);
+	}
+};
+
+vector<int> implement_LRU_cache(int capacity, vector<int> query_type, vector<int> key, vector<int> value)
+{
+	int n = query_type.size();
+	// Setup cache. 
+	LRU_cache* cache = new LRU_cache(capacity);
+	vector<int> ans;
+	for (int i = 0; i < n; i++)
+	{
+		if (query_type[i] == 0)
+		{
+			ans.push_back(cache->get(key[i]));
+		}
+		else
+		{
+			cache->set(key[i], value[i]);
+		}
+	}
+	return ans;
+}
+
+// -------------------------- STOP ---------------------------
+
+int main()
+{
+	freopen("..//test_cases//sample_test_cases_input.txt", "r", stdin);
+	freopen("..//test_cases//sample_test_cases_expected_output.txt", "w", stdout);
+	//freopen("..//test_cases//handmade_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//handmade_test_cases_expected_output.txt", "w", stdout);
+	//freopen("..//test_cases//generated_small_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//generated_small_test_cases_expected_output.txt", "w", stdout);
+	//freopen("..//test_cases//generated_big_test_cases_input.txt", "r", stdin);
+	//freopen("..//test_cases//generated_big_test_cases_expected_output.txt", "w", stdout);
+	//freopen("..//test_cases//ignore.txt", "w", stdout);
+
+	int test_cases;
+	cin >> test_cases;
+	assert(test_cases >= 0);
+	while (test_cases--)
+	{
+		int capacity;
+		cin >> capacity;
+		assert(1 <= capacity);
+		assert(capacity <= MAX_CAPACITY);
+
+		int n;
+		cin >> n;
+		assert(1 <= n);
+		assert(n <= MAX_N);
+		vector<int> query_type(n);
+		int no_of_get_queries = 0;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> query_type[i];
+			no_of_get_queries += (query_type[i] == 0);
+			assert(query_type[i] == 0 || query_type[i] == 1);
+		}
+
+		int temp_n;
+		cin >> temp_n;
+		assert(n == temp_n);
+		vector<int> key(n);
+		for (int i = 0; i < n; i++)
+		{
+			cin >> key[i];
+			assert(1 <= key[i]);
+			assert(key[i] <= MAX_KEY);
+		}
+
+		cin >> temp_n;
+		assert(n == temp_n);
+		vector<int> value(n);
+		for (int i = 0; i < n; i++)
+		{
+			cin >> value[i];
+			assert(1 <= value[i]);
+			assert(value[i] <= MAX_VAL);
+		}
+
+		vector<int> ans = implement_LRU_cache(capacity, query_type, key, value);
+		int len = ans.size();
+		assert(len == no_of_get_queries);
+		for (int i = 0; i < len; i++)
+		{
+			cout << ans[i] << endl;
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
---
+++ Linked_Lists_Stacks_Queues/Design_And_Implement_LRU_Cache/test_cases_generators/big_test_cases_generator.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// ----------------------------------------------------------------------
+
+long long int MAX_MOD = 1000000000000000000LL;
+
+// Random number in range [0, mod). Mod should be <= 10^18.
+
+long long int random_number(long long int mod)			
+{
+	assert(1 <= mod);
+	assert(mod <= MAX_MOD);
+	if (RAND_MAX == 32767)
+	{
+		long long int no = rand();	
+		no |= ((long long int)rand() << 15LL);
+		no |= ((long long int)rand() << 30LL);
+		no |= ((long long int)rand() << 45LL);
+		no |= ((long long int)rand() << 47LL);
+		return no % mod;
+	}
+	else if (RAND_MAX == 2147483647)
+	{
+		long long int no = rand();
+		no |= ((long long int)rand() << 31LL);
+		return no % mod;  
+	}
+	assert(false);
+	return -1;
+}
+
+// --------------------------------------------------------------------------
+
+const int MAX_CAPACITY = 100000, MAX_N = 100000, MAX_KEY = 10000, MAX_VAL = 10000;
+
+void generate_test_case(int &test_cases, int n = MAX_N, int max_capacity = MAX_CAPACITY, int max_key = MAX_KEY, int max_val = MAX_VAL)
+{
+	test_cases--;
+	cout << endl;
+
+	int capacity = random_number(max_capacity) + 1;
+	cout << capacity << endl;
+	cout << n << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << random_number(2) << endl;
+	}
+	cout << n << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << random_number(max_key) + 1 << endl;
+	}
+	cout << n << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << random_number(max_val) + 1 << endl;
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	freopen("..//test_cases//generated_big_test_cases_input.txt", "w", stdout);
+	//freopen("..//test_cases//ignore.txt", "w", stdout);
+
+	int test_cases = 27;
+	cout << test_cases << endl;
+	generate_test_case(test_cases, 15, 3, 5, 5);
+	generate_test_case(test_cases, 18, 3, 5, 5);
+	for (int i = 0; i < 23; i++)
+	{
+		generate_test_case(test_cases, 500 + i, 50 + i, 50 + i, 50 + i);
+	}
+	generate_test_case(test_cases);
+	generate_test_case(test_cases);
+
+	assert(test_cases == 0);
+
+	return 0;
+}
