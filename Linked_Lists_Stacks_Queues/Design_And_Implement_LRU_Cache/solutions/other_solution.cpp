104326eb8817dd0fa7470f3090b4445e3307f602
dipen <dipendadhaniya@gmail.com>
2018-01-25 10:01:48 +0530

    Updating problem "Design_And_Implement_LRU_Cache" in "Linked_Lists_Stacks_Queues".
    Adding one more solution using hash map + heap.

--- Linked_Lists_Stacks_Queues/Design_And_Implement_LRU_Cache/solutions/other_solution.cpp
+++ Linked_Lists_Stacks_Queues/Design_And_Implement_LRU_Cache/solutions/other_solution.cpp
@@ -9,86 +9,79 @@ const int MAX_CAPACITY = 100000, MAX_N = 100000, MAX_KEY = 10000, MAX_VAL = 1000
 class LRU_cache
 {
 	// Maximum size of the cache.
-	int capacity;
-	/*
-	Stores the {key, value} pair.
-	Implementing own linked list will give some speedup than using built-in list, but it will affect the readibility of the code, hence we have used built in list.
-	*/
-	list<pair<int, int>> actual_storage;
-	// key -> pointer to where it is stored in actual storage.
-	unordered_map<int, list<pair<int, int>>::iterator> key_to_actual_storage_mapping;
+	int capacity = 0;
+	int time_stamp = 0;
+	// Heap storing {time_stamp, key} pairs.
+	vector<pair<int, int>> lru_heap;
+	// Hash map storing {key, {time_stamp, value}} pairs.
+	unordered_map<int, pair<int, int>> cache;
 
 public:
-
+	
 	// Constructor.
-	LRU_cache(int _capacity)
+	LRU_cache(int _capacity) 
 	{
 		capacity = _capacity;
 	}
 
-	/*
-	Add {key, value} pair at the front of actual_storage. 
-	Also add the mapping (key -> pointer to where it is stored in actual storage).
-	That is (key -> beginning of the actual_storage).
-	*/
-	void add_to_front(int key, int value)
-	{
-		actual_storage.push_front({key, value});
-		key_to_actual_storage_mapping[key] = actual_storage.begin();
-	}
-
-	/*
-	Remove one {key, value} pair from the end of actual_storage.
-	Also remove the mapping (key -> pointer to where it is stored in actual storage).
-	*/
-	void remove_least_recently_used()
-	{
-		int key = actual_storage.back().first;
-		key_to_actual_storage_mapping.erase(key);
-		actual_storage.pop_back();
-	}
-
 	// Return value of the key, if key is present, else return -1.
-	int get(int key)
+	int get(int key) 
 	{
-		unordered_map<int, list<pair<int, int>>::iterator>::iterator it = key_to_actual_storage_mapping.find(key);
-		// If key is not present in mapping then return -1.
-		if (it == key_to_actual_storage_mapping.end())
+		// If the key is already present.
+		if(cache.find(key) != cache.end()) 
+		{
+			// Update the timestamp in the hash map to current timestamp.
+			cache[key].first = time_stamp--;
+			return cache[key].second;
+		} 
+		else 
 		{
 			return -1;
 		}
-		/*
-		it->second points to {key, value} in the actual_storage.
-		So it->second->second will give value.
-		*/
-		int value = it->second->second;
-		// Remove from the original position.
-		actual_storage.erase(it->second);
-		// Add to the front.
-		add_to_front(key, value);
-		return value;
 	}
 
 	// If key is present in cache then updates its values, else add {key, value} pair in cache.
-	void set(int key, int value)
+	void set(int key, int value) 
 	{
-		unordered_map<int, list<pair<int, int>>::iterator>::iterator it = key_to_actual_storage_mapping.find(key);
-		// If key is not present in mapping then add {key, value} and setup the mapping.
-		if (it == key_to_actual_storage_mapping.end())
+		// Take care of the case where key is already in the cache.
+		if(cache.find(key)!=cache.end()) 
 		{
-			// If cache is full then remove the least recently used {key, value}.
-			if (actual_storage.size() == capacity)
-			{
-				remove_least_recently_used();
-			}
-			// Add to the front.
-			add_to_front(key, value);
-			return;
+	  		cache[key]={time_stamp--, value};
+	  		return;
 		}
-		// Remove from the original position.
-		actual_storage.erase(it->second);
-		// Add to the front.
-		add_to_front(key, value);
+		// Remove least recently used element if cache is full.
+		if(lru_heap.size() == capacity)
+		{
+			// Loop till we have removed one element from cache.
+		  	while(true) 
+		  	{
+			    // Remove from heap. Removed element will be at the back of vector.
+			    pop_heap(lru_heap.begin(), lru_heap.end());
+			    // Get the removed element from back.
+			    auto& top = lru_heap.back();
+			    // Check timestamp in hash map.
+			    if(cache[top.second].first == top.first) 
+			    {
+				    // Element has same time stamp in heap and hash map hence we have found the element to be removed.
+				    // Remove it from hash map.
+				    cache.erase(top.second);
+				    // Remove from heap.
+				    lru_heap.pop_back();
+				    break;
+			    }
+			    else 
+			    {
+			    	// This key was updated, hence push it back in heap with updated time stamp.
+			    	top.first = cache[top.second].first;
+			    	push_heap(lru_heap.begin(), lru_heap.end());
+			    }
+		  	}
+		}
+		// Add the new key into hash map and heap with the appropriate timestamp.
+		cache[key] = {time_stamp, value};
+		lru_heap.push_back({time_stamp, key});
+		push_heap(lru_heap.begin(), lru_heap.end());
+		time_stamp--;
 	}
 };
 
