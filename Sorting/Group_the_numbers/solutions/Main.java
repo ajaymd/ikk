6d8bdd4224b7978b4f9f794797be46d79b8a09a0
akshaymiterani <akshaymiterani@gmail.com>
2018-02-06 00:55:49 +0530

    Adding commented solution and detailed editorial - HW Sorting-1

--- Sorting/Group_the_numbers/solutions/Main.java
+++ Sorting/Group_the_numbers/solutions/Main.java
@@ -1,17 +1,26 @@
 public class Main {
 	static int[] solve(int[] arr) {
 		int n = arr.length;
+		// Initialize left pointer to the start of the array and right pointer to the end of the array
 		int left = 0, right = n - 1;
+		// Move the pointers till they cross each other 
 		while (right >= left) {
 			if (arr[right] % 2 == 0) {
 				int tmp = arr[right];
 				arr[right] = arr[left];
 				arr[left] = tmp;
+				// left will always be increased and all values to the left of the left pointer will be even
 				left++;
 			} else {
+				// right will always be decreased and all values to the right of the right pointer will be odd
 				right--;
 			}
 		}
+		/*
+		 * Our solution is in-place solution, as we are making changes in the input array
+		 * Time Complexity: O(n)
+		 * Space Complexity: O(1) 
+		 */
 		return arr;
 	}
 }
