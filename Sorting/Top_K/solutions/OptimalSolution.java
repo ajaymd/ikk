34ca167f5a92450012a717477aaa9cbf3c146753
akshaymiterani <akshaymiterani@gmail.com>
2018-02-11 13:09:30 +0530

    Updating Top_K

---
+++ Sorting/Top_K/solutions/OptimalSolution.java
@@ -0,0 +1,25 @@
+import java.util.TreeSet;
+
+public class OptimalSolution {
+    /*
+     * Time Complexity: O(N*log(K))
+     * Space Complexity: O(K)
+     */
+	static int[] topK(int[] arr, int k) {
+        // TreeSet will maintain set of elements in a sorted fashion
+		TreeSet<Integer> tree = new TreeSet<Integer>();
+        // We will add all elements to the sorted set and when size of the set increases over required size K, we will remove the smallest element
+		for (int x : arr) {
+			tree.add(x);
+			if (tree.size() > k) {
+				tree.pollFirst();
+			}
+		}
+		int ans[] = new int[tree.size()];
+		int ptr = 0;
+		for (int x : tree) {
+			ans[ptr++] = x;
+		}
+		return ans;
+	}
+}
