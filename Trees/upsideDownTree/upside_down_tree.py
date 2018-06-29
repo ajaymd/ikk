e9ef18f31a6cbf6152cad1963383c8d841f33302
afzal273 <afzal273@users.noreply.github.com>
2018-06-28 22:24:07 -0700

    upside down tree

---
+++ Trees/upsideDownTree/upside_down_tree.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/binary-tree-upside-down/description/
+# This question is very similar to recursive reversing of a linked list - https://leetcode.com/problems/reverse-linked-list/description/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def upsideDownBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root or not root.left:
+            return root
+        res = self.upsideDownBinaryTree(root.left)
+        root.left.right = root
+        root.left.left = root.right
+        root.left = None
+        root.right = None
+        return res
